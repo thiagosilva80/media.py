22afa73180263d8e558175253b79c3364b8ef9f4
thiagosilva80 <148148163+thiagosilva80@users.noreply.github.com>
2023-11-27 16:11:52 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,11 @@
+n1= float(input("insira a nota: "))
+n2= float(input("insira a nota: "))
+n3= float(input("insira a nota: "))
+n4= float(input("insira a nota: "))
+
+media = (n1+n2+n3+n4) /4
+if media >= 7:
+    print("aprovado")
+
+else:
+    print("reprovado")
